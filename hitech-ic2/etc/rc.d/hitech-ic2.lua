e06a8a6fc45a080d68b89a4a3bbe63b2ea70659c
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2020-06-07 12:38:34 +0200

    wip

--- hitech-ic2/etc/rc.d/hitech-ic2.lua
+++ hitech-ic2/etc/rc.d/hitech-ic2.lua
@@ -1,7 +1,15 @@
+local running = false;
+
 function start()
+    running = true;
     print('starting hitech-ic2 server...')
 end
 
 function stop()
+    running = false;
     print('stopping hitech-ic2 server...')
+end
+
+function status()
+    print ('status of hitech-ic2 is ' .. running and 'running' or 'not running')
 end
